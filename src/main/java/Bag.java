202e9032cabf4aed2b0f088970a48746399b5d23
John Hunter <john@pushagency.io>
2016-11-04 10:03:00 -0700

    initial commit

---
+++ src/main/java/Bag.java
@@ -0,0 +1,11 @@
+import java.util.Iterator;
+
+/**
+ * Created by John on 11/4/16.
+ */
+public class Bag<Item> implements Iterable<Item> {
+
+    public Iterator<Item> iterator() {
+        return null;
+    }
+}
---
+++ src/main/java/Item.java
@@ -0,0 +1,5 @@
+/**
+ * Created by John on 11/4/16.
+ */
+public class Item {
+}
